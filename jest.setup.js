c387a7e5bbe54edf8bf2f9e30a4a1d01935bca79
José Delpino <delpinoivivas@gmail.com>
2024-05-20 16:03:16 -0500

    refactor: Comment out console spy setup and restore in jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,13 +1,13 @@
-jest.setup.js;
+// jest.setup.js;
 
-beforeAll(() => {
-  jest.spyOn(console, "error").mockImplementation(() => {});
-  jest.spyOn(console, "warn").mockImplementation(() => {});
-  jest.spyOn(console, "log").mockImplementation(() => {});
-});
+// beforeAll(() => {
+//   // jest.spyOn(console, "error").mockImplementation(() => {});
+//   // jest.spyOn(console, "warn").mockImplementation(() => {});
+//   // jest.spyOn(console, "log").mockImplementation(() => {});
+// });
 
-afterAll(() => {
-  console.error.mockRestore();
-  console.warn.mockRestore();
-  console.log.mockRestore();
-});
+// afterAll(() => {
+//   // console.error.mockRestore();
+//   // console.warn.mockRestore();
+//   // console.log.mockRestore();
+// });
